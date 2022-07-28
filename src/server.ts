cb59c0150ebe9c9c1639b9d68c162ab2f9f9809f
heudersena <documento_arquivo@hotmail.com>
2022-07-28 16:35:02 -0300

    reconfigurando o graphql

--- src/server.ts
+++ src/server.ts
@@ -18,21 +18,11 @@ interface Context {
     prisma: PrismaClient;
 }
 
-import { Assinature_email } from "../prisma/generated/type-graphql";
-
-@Resolver((of) => Assinature_email)
-class AssinaturesEmail {
-    @Query((returns) => Assinature_email, { nullable: true })
-    async bestAssinature(
-        @Ctx() { prisma }: Context
-    ): Promise<Assinature_email[] | []> {
-        return await prisma.assinature_email.findMany({});
-    }
-}
+import { resolvers } from "../prisma/generated/type-graphql";
 
 async function main() {
     const schema = await buildSchema({
-        resolvers: [AssinaturesEmail],
+        resolvers: resolvers,
         emitSchemaFile: path.resolve(__dirname, "./generated-schema.graphql"),
         validate: false,
     });
