136354fb9bb62a30657f67c06f4710a3ac634b4a
heudersena <documento_arquivo@hotmail.com>
2022-07-29 20:47:51 -0300

    update

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,7 @@
 import "reflect-metadata";
 import path from "path";
 
-import {
-    Resolver,
-    Query,
-    buildSchema,
-    FieldResolver,
-    Ctx,
-    Root,
-} from "type-graphql";
+import { buildSchema } from "type-graphql";
 
 import { ApolloServer } from "apollo-server";
 
@@ -22,7 +15,7 @@ import { resolvers } from "../prisma/generated/type-graphql";
 
 async function main() {
     const schema = await buildSchema({
-        resolvers: resolvers,
+        resolvers: resolvers, // see below for more about this
         emitSchemaFile: path.resolve(__dirname, "./generated-schema.graphql"),
         validate: false,
     });
@@ -32,7 +25,13 @@ async function main() {
     const server = new ApolloServer({
         schema,
         context: (): Context => ({ prisma }),
+        cors: { origin: ["10.67.11.2", "https://studio.apollographql.com"] },
+        csrfPrevention: true,
+        cache: "bounded",
     });
+
+    server.graphqlPath = "/graphql";
+
     const { port } = await server.listen(4000);
     console.log(`GraphQL is listening on ${port}!`);
 }
